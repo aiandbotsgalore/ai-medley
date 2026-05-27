6339f200399fe5a4b2b3641ded9e46a6d362dd97
aiandbotsgalore <thekratlife@gmail.com>
2026-05-27 01:19:36 -0700

    chore: create initial runAutonomousLoop.ts file for refactoring

---
+++ src/engine/runAutonomousLoop.ts
@@ -0,0 +1,7 @@
+// This file will contain the extracted autonomous loop logic.
+// Starting point for refactoring App.tsx
+
+export async function runAutonomousLoop() {
+  // Core agent loop will be moved here
+  console.log('Autonomous loop placeholder');
+}
